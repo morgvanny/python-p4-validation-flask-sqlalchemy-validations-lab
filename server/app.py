b2c26dff10d4b0119d12dfc7010abf8bf47896b0
Morgan VanYperen <morgvanny@gmail.com>
2024-01-12 09:21:33 -0800

    done

--- server/app.py
+++ server/app.py
@@ -1,19 +1,40 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
-
-from models import db, Author, Post
+from flask_restful import Api, Resource
+from models import Author, db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def index():
-    return 'Validations lab'
+api = Api(app)
 
-if __name__ == '__main__':
+
+@app.route("/")
+def index():
+    return "Validations lab"
+
+
+class Authors(Resource):
+    def post(self):
+        data = request.get_json()
+        author = Author()
+        try:
+            for attr in data:
+                setattr(author, attr, data[attr])
+            author.name = data.get("name")
+            db.session.add(author)
+            db.session.commit()
+            return make_response(author.to_dict(), 201)
+        except ValueError as e:
+            return make_response({"error": e.__str__()}, 400)
+
+
+api.add_resource(Authors, "/authors")
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,24 +1,43 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
+
 db = SQLAlchemy()
 
-class Author(db.Model):
-    __tablename__ = 'authors'
-    
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = "authors"
+
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    # Add validators
+
+    @validates("name")
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError("Author must have a name.")
+        author = type(self).query.filter_by(name=value).first()
+        if author:
+            raise ValueError("Author names must be unique.")
+        return value
+
+    @validates("phone_number")
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError("Phone numbers must have 10 digits.")
+        return phone_number
 
     def __repr__(self):
-        return f'Author(id={self.id}, name={self.name})'
+        return f"Author(id={self.id}, name={self.name})"
+
 
 class Post(db.Model):
-    __tablename__ = 'posts'
-    
+    __tablename__ = "posts"
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.String)
@@ -27,8 +46,31 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    @validates("content")
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Post content must be at least 250 characters.")
+        return content
 
+    @validates("summary")
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError("Post summary must be under 250 characters.")
+        return summary
+
+    @validates("category")
+    def validate_category(self, key, category):
+        if category != "Fiction" and category != "Non-Fiction":
+            raise ValueError("Category must be Fiction or Non-Fiction.")
+        return category
+
+    @validates("title")
+    def validate_title(self, key, title):
+        click_baits = ["Won't Believe", "Secret", "Top", "Guess"]
+        for bait in click_baits:
+            if bait in title:
+                return title
+        raise ValueError("Not clickbait-y enough.")
 
     def __repr__(self):
-        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+        return f"Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})"
